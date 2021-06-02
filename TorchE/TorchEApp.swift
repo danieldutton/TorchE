55b2da732219f5f39d899d4d99c757d2e0fda580
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-02 19:30:05 +0100

    Initial Commit

---
+++ TorchE/TorchEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorchEApp.swift
+//  TorchE
+//
+//  Created by Daniel Dutton on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TorchEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
