c7229e853fc71ce65753a165a9753015630f04f0
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-03 22:40:24 +0100

    Round 1 of initial UI work
    Just rough stuff to work with.
    Needs much refinement.

--- TorchE/View/Torch.swift
+++ TorchE/View/Torch.swift
@@ -1,14 +1,43 @@
+import AVFoundation
 import SwiftUI
 
 struct Torch: View {
+    @State var isOn = false
+    @State var sliderValue = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                GeometryReader { geom in
+                        Slider(value: $sliderValue)
+                            .padding()
+                            .padding()
+                    VStack(alignment: .center) {
+                    
+                        Image("Torch_Off")
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(radius: 8)
+                        .padding()
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.yellow)
+                                .cornerRadius(45.0, antialiased: true)
+                                //.shadow(radius: 30)
+                            Toggle("", isOn: $isOn)
+                                .frame(alignment: .center)
+                        }
+                    }
+            }
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Torch_Previews: PreviewProvider {
     static var previews: some View {
         Torch()
     }
 }
+
+
