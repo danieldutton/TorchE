e8711e8bee53681d05e7fb18584ba6baeafcb2f4
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-03 23:00:58 +0100

    Add Flashlight code and wire up

--- TorchE/View/Torch.swift
+++ TorchE/View/Torch.swift
@@ -27,11 +27,36 @@ struct Torch: View {
                                 //.shadow(radius: 30)
                             Toggle("", isOn: $isOn)
                                 .frame(alignment: .center)
+                                .onChange(of: isOn, perform: { value in
+                                    toggleTorch(on: isOn)
+                                })
                         }
                     }
             }
         }.edgesIgnoringSafeArea(.all)
     }
+    
+func toggleTorch(on: Bool) {
+    guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+    if device.hasTorch {
+        do {
+            try device.lockForConfiguration()
+
+            if on == true {
+                device.torchMode = .on
+            } else {
+                device.torchMode = .off
+            }
+
+            device.unlockForConfiguration()
+        } catch {
+            print("Torch could not be used")
+        }
+    } else {
+        print("Torch is not available")
+    }
+ }
 }
 
 struct Torch_Previews: PreviewProvider {
