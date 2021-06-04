075e0d53125d206c5679c43f3f5db24559081f53
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-04 11:28:23 +0100

    Use 'GeometryReader' to space stacks accordingly

--- TorchE/View/TorchView.swift
+++ TorchE/View/TorchView.swift
@@ -10,10 +10,10 @@ struct TorchView: View {
             Color.blue
                 GeometryReader { geom in
                     VStack(alignment: .center) {
-                    
                         Image(isOn ? "Torch_On" : "Torch_Off")
                         .resizable()
                         .scaledToFit()
+                            .frame(height: geom.size.height / 1.6)
                         .shadow(radius: 8)
                         .padding()
                         
